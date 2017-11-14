909846371f7f381f82f6e1f737560f5388a6f877
Nick Marley <nickmarley@my.smccd.edu>
2017-11-13 19:55:40 -0800

    First release
    might not even work

---
+++ ATCrackExtGUI/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace ATCrackExtGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.licenseBox = new System.Windows.Forms.TextBox();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.optifineBox = new System.Windows.Forms.TextBox();
+            this.hypixelBox = new System.Windows.Forms.TextBox();
+            this.usernamesBtn = new System.Windows.Forms.Button();
+            this.optifineBtn = new System.Windows.Forms.Button();
+            this.hypixelBtn = new System.Windows.Forms.Button();
+            this.launchBtn = new System.Windows.Forms.Button();
+            this.openUsernames = new System.Windows.Forms.OpenFileDialog();
+            this.saveOptifine = new System.Windows.Forms.SaveFileDialog();
+            this.saveHypixel = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // licenseBox
+            // 
+            this.licenseBox.Location = new System.Drawing.Point(48, 134);
+            this.licenseBox.Name = "licenseBox";
+            this.licenseBox.Size = new System.Drawing.Size(153, 20);
+            this.licenseBox.TabIndex = 0;
+            this.licenseBox.Text = "License";
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(12, 12);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.ReadOnly = true;
+            this.usernameBox.Size = new System.Drawing.Size(115, 20);
+            this.usernameBox.TabIndex = 1;
+            this.usernameBox.Text = "usernames.txt";
+            // 
+            // optifineBox
+            // 
+            this.optifineBox.Location = new System.Drawing.Point(14, 49);
+            this.optifineBox.Name = "optifineBox";
+            this.optifineBox.ReadOnly = true;
+            this.optifineBox.Size = new System.Drawing.Size(115, 20);
+            this.optifineBox.TabIndex = 2;
+            this.optifineBox.Text = "optifine.txt";
+            // 
+            // hypixelBox
+            // 
+            this.hypixelBox.Location = new System.Drawing.Point(15, 91);
+            this.hypixelBox.Name = "hypixelBox";
+            this.hypixelBox.ReadOnly = true;
+            this.hypixelBox.Size = new System.Drawing.Size(114, 20);
+            this.hypixelBox.TabIndex = 3;
+            this.hypixelBox.Text = "hypixel.txt";
+            // 
+            // usernamesBtn
+            // 
+            this.usernamesBtn.Location = new System.Drawing.Point(135, 12);
+            this.usernamesBtn.Name = "usernamesBtn";
+            this.usernamesBtn.Size = new System.Drawing.Size(101, 23);
+            this.usernamesBtn.TabIndex = 4;
+            this.usernamesBtn.Text = "Select Usernames";
+            this.usernamesBtn.UseVisualStyleBackColor = true;
+            this.usernamesBtn.Click += new System.EventHandler(this.usernamesBtn_Click);
+            // 
+            // optifineBtn
+            // 
+            this.optifineBtn.Location = new System.Drawing.Point(135, 41);
+            this.optifineBtn.Name = "optifineBtn";
+            this.optifineBtn.Size = new System.Drawing.Size(101, 35);
+            this.optifineBtn.TabIndex = 5;
+            this.optifineBtn.Text = "Optifine Save Locaiton";
+            this.optifineBtn.UseVisualStyleBackColor = true;
+            this.optifineBtn.Click += new System.EventHandler(this.optifineBtn_Click);
+            // 
+            // hypixelBtn
+            // 
+            this.hypixelBtn.Location = new System.Drawing.Point(135, 82);
+            this.hypixelBtn.Name = "hypixelBtn";
+            this.hypixelBtn.Size = new System.Drawing.Size(101, 36);
+            this.hypixelBtn.TabIndex = 6;
+            this.hypixelBtn.Text = "Hypixel Save Location";
+            this.hypixelBtn.UseVisualStyleBackColor = true;
+            this.hypixelBtn.Click += new System.EventHandler(this.hypixelBtn_Click);
+            // 
+            // launchBtn
+            // 
+            this.launchBtn.Location = new System.Drawing.Point(14, 169);
+            this.launchBtn.Name = "launchBtn";
+            this.launchBtn.Size = new System.Drawing.Size(221, 55);
+            this.launchBtn.TabIndex = 7;
+            this.launchBtn.Text = "Launch ATCrack External!";
+            this.launchBtn.UseVisualStyleBackColor = true;
+            this.launchBtn.Click += new System.EventHandler(this.launchBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 240);
+            this.Controls.Add(this.launchBtn);
+            this.Controls.Add(this.hypixelBtn);
+            this.Controls.Add(this.optifineBtn);
+            this.Controls.Add(this.usernamesBtn);
+            this.Controls.Add(this.hypixelBox);
+            this.Controls.Add(this.optifineBox);
+            this.Controls.Add(this.usernameBox);
+            this.Controls.Add(this.licenseBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ATCrack External GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox licenseBox;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox optifineBox;
+        private System.Windows.Forms.TextBox hypixelBox;
+        private System.Windows.Forms.Button usernamesBtn;
+        private System.Windows.Forms.Button optifineBtn;
+        private System.Windows.Forms.Button hypixelBtn;
+        private System.Windows.Forms.Button launchBtn;
+        private System.Windows.Forms.OpenFileDialog openUsernames;
+        private System.Windows.Forms.SaveFileDialog saveOptifine;
+        private System.Windows.Forms.SaveFileDialog saveHypixel;
+    }
+}
+
---
+++ ATCrackExtGUI/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATCrackExtGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        readonly string externalPath = Environment.CurrentDirectory + "/External.jar";
+        readonly string licensePath = Environment.CurrentDirectory + "/license.txt";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(licensePath))
+            {
+                File.Create(licensePath);
+            }
+            if (File.Exists(licensePath))
+            {
+                licenseBox.Text = File.ReadAllText(licensePath) == "" ? "License" : File.ReadAllText(licensePath);
+            }
+            if (File.Exists(externalPath)) return;
+            MessageBox.Show("External.jar not found!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            Environment.Exit(0);
+        }
+
+        private void usernamesBtn_Click(object sender, EventArgs e)
+        {
+            if (openUsernames.ShowDialog() == DialogResult.OK)
+            {
+                usernameBox.Text = Path.GetFileName(openUsernames.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Oops! An error has occured.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void optifineBtn_Click(object sender, EventArgs e)
+        {
+            if (saveOptifine.ShowDialog() == DialogResult.OK)
+            {
+                optifineBox.Text = Path.GetFileName(saveOptifine.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Oops! An error has occured.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void hypixelBtn_Click(object sender, EventArgs e)
+        {
+            if (saveHypixel.ShowDialog() == DialogResult.OK)
+            {
+                hypixelBox.Text = Path.GetFileName(saveHypixel.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Oops! An error has occured.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void launchBtn_Click(object sender, EventArgs e)
+        {
+            if (licenseBox.TextLength != 24)
+            {
+                MessageBox.Show("Invalid License!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            File.WriteAllText(licensePath, licenseBox.Text);
+            if (openUsernames.FileName == "")
+            {
+                MessageBox.Show("Invalid Username file!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(saveHypixel.FileName) && string.IsNullOrEmpty(saveOptifine.FileName))
+            {
+                MessageBox.Show("You must select a Save Location for Optifine or Hypixel!", "Error!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string args = " -jar External.jar -l " + licenseBox.Text + " -u " + openUsernames.FileName;
+            if (!string.IsNullOrEmpty(saveHypixel.FileName))
+            {
+                args += " -h " + saveHypixel.FileName;
+            }
+            if (!string.IsNullOrEmpty(saveOptifine.FileName))
+            {
+                args += " -o " + saveOptifine.FileName;
+            }
+            if (RunningCheck())
+            {
+                MessageBox.Show(args);
+                Process.Start("java", args);
+                Environment.Exit(0);
+            }
+        }
+
+        private bool RunningCheck()
+        {
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                if (p.MainWindowTitle.Contains("Accounts Today External"))
+                {
+                    DialogResult dialogResult = MessageBox.Show(
+                        "ATCrack External is already running. Would you like me to shut it down?", "Error!",
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        p.Kill();
+                        return true;
+                    }
+                    if (dialogResult != DialogResult.Yes)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
